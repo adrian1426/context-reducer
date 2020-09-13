d16e4d15d507d848cc17a7ae36ce288bb735ac54
Adrian1426 <adrianhh1426@gmail.com>
2020-09-13 13:52:53 -0500

    consumiendo context con hook useContext

--- src/components/ComponenteHijo.js
+++ src/components/ComponenteHijo.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const ComponenteHijo = () => {
+  const valueContextGlobal = useContext(AppContext);
+
   return (
-    <AppContext.Consumer>
-      {(valueContextGlobal) => (
-        <div>
-          <h1>click: ,{valueContextGlobal.click}</h1>
-          <button onClick={valueContextGlobal.add}>+</button>
-          <button onClick={valueContextGlobal.remove}>-</button>
-        </div>
-      )}
-    </AppContext.Consumer>
+    <div>
+      <h1>click: ,{valueContextGlobal.click}</h1>
+      <button onClick={valueContextGlobal.add}>+</button>
+      <button onClick={valueContextGlobal.remove}>-</button>
+    </div>
   );
 };
 
