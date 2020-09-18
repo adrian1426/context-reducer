ef24f52a351dc58e9e21ef0072bf310e88710c83
Adrian1426 <adrianhh1426@gmail.com>
2020-09-17 22:10:54 -0500

    useContext

--- src/components/useContext/HomeScreen.js
+++ src/components/useContext/HomeScreen.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 const HomeScreen = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <div>
       <h1>Home</h1>
+      <pre>
+        {
+          JSON.stringify(user, null, 3)
+        }
+      </pre>
     </div>
   );
 };
--- src/components/useContext/LoginScreen.js
+++ src/components/useContext/LoginScreen.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
 
 const LoginScreen = () => {
+  const { setUser } = useContext(UserContext);
+
   return (
     <div>
       <h1>Login</h1>
+      <button onClick={() => setUser({ id: 1, name: 'Adrian', apellido: 'hdz' })}>
+        agregar usario
+        </button>
     </div>
   );
 };
